6da0122a141ae0f4202ffeae3c903701cbca6dc2
mineult <tle57@gmu.edu>
2024-09-19 21:26:42 -0400

    m4

--- module4/groupAssignment/src/Queue.java
+++ module4/groupAssignment/src/Queue.java
@@ -1,33 +1,60 @@
+/**
+ * Matthew Lauer - 100%
+ * Dan Le - 100%
+ * Sophia Nadasy - 100%
+ * Sri Teja Kale - 100%
+ * Sri Bhuvan Mad
+ */
 import java.util.*;
-public class Queue<E>  {
-   private List<E> elements;
-   private int size;
-   public Queue() {   
-      this.elements = new ArrayList<>();
-      this.size = 0;
-   }
-   public void enQueue (E e) {
-     elements.add(e);
-     size++;
-   }
-   public E deQueue () {
-     if (size == 0) throw new IllegalStateException("Queue.deQueue");
-     E result = elements.get(0);
-     elements.remove(0);
-     size--;
-     return result;
-   }
-   public boolean isEmpty() {
-      return size == 0;
-   }
+
+public final class Queue<E> {
+
+  private final List<E> elements;
+
+  public Queue() {
+    this.elements = Collections.unmodifiableList(new ArrayList<>());
+  }
+
+  private Queue(List<E> elements) {
+    ArrayList<E> newList = new ArrayList<>(elements);
+    this.elements = Collections.unmodifiableList(newList);
+  }
+
+  public Queue<E> enQueue(E e) { //Observer
+    List<E> newList = new ArrayList<>(this.elements);
+    newList.add(e);
+    return new Queue<>(newList);
+  }
+
+  public Queue<E> deQueue() {
+    if (isEmpty()) {
+      throw new IllegalStateException("Queue.deQueue");
+    }
+    List<E> newList = new ArrayList<>(this.elements);
+    newList.remove(0);
+    return new Queue<>(newList);
+  }
+
+  public E peek() {
+    if (isEmpty()) {
+      throw new IllegalStateException("Queue.peek");
+    }
+    E result = elements.get(0); //observer
+    return result; //observer
+  }
+
+  public boolean isEmpty() {
+    return this.elements.isEmpty();
+  }
+
   public static void main(String[] args) {
-     // Simple exercise to enQueue/deQueue cmd line args
-     // Usage:  java Queue item1 item2 item3 ...
-     String [] strings = {"item1", "item2", "item3"};
-     Queue<String>  q = new Queue<> ();
-     for (String string : strings)
-        q.enQueue(string);
-     while (!q.isEmpty() )
-        System.out.println(q.deQueue());
+    // Simple exercise to enQueue/deQueue cmd line args
+    // Usage:  java Queue item1 item2 item3 ...
+    Queue<String> q = new Queue<>();
+    for (String arg : args) q = q.enQueue(arg);
+    while (!q.isEmpty()) {
+      System.out.println(q.peek());
+      q = q.deQueue();
+    }
   }
 }
